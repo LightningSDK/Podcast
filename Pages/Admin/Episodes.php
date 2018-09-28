8586c2d853e0495a37252642e53e00e4d90a2f70
DAB <dab@dab.la>
2018-09-27 23:04:57 -0500

    Added duration to feed

--- Pages/Admin/Episodes.php
+++ Pages/Admin/Episodes.php
@@ -5,6 +5,7 @@ namespace Modules\Podcast\Pages\Admin;
 use Lightning\Pages\Table;
 use Lightning\Tools\ClientUser;
 use Lightning\Tools\Configuration;
+use Lightning\Tools\IO\FileManager;
 
 class Episodes extends Table {
     const TABLE = 'podcast_episode';
@@ -16,6 +17,18 @@ class Episodes extends Table {
             'type' => 'file',
             'replace' => true,
         ],
+        'duration' => [
+            'type' => 'hidden',
+        ],
+    ];
+
+    protected $fieldOrder = [
+        'date',
+        'title',
+        'description',
+        'keywords',
+        'file',
+        'duration',
     ];
 
     public function hasAccess() {
@@ -25,5 +38,26 @@ class Episodes extends Table {
     public function initSettings() {
         parent::initSettings();
         $this->preset['file']['container'] = Configuration::get('modules.podcast.container');
+    }
+
+    /**
+     * @TODO: This should be moved to a media module
+     */
+    protected function processFieldValues(&$row) {
+        $fileHandler = FileManager::getFileHandler(Configuration::get('modules.podcast.filehandler'), Configuration::get('modules.podcast.container'));
+        $localFile = $fileHandler->getAbsoluteLocal($row['file']);
+
+        $output = shell_exec('ffmpeg -i ' . escapeshellarg($localFile) . ' 2>&1');
+        $output = explode("\n", $output);
+        foreach ($output as $line) {
+            $matches = [];
+            if (preg_match('/Duration: ([0-9:.]+)/', $line, $matches)) {
+                $duration = explode(':', $matches[1]);
+                $row['duration'] = (intval($duration[0]) * 3600) + (intval($duration[1]) * 60) + intval($duration[2]);
+                return true;
+            }
+        }
+
+        return true;
     }
 }
