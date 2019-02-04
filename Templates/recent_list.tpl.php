1057930ae889cfcf1c4c8fa7b12c7ebf28fa9d3a
DAB <dab@dab.la>
2019-02-04 09:40:08 -0500

    fixed podcast recent links

--- Templates/recent_list.tpl.php
+++ Templates/recent_list.tpl.php
@@ -2,7 +2,7 @@
 <?php if (!empty($episodes)): ?>
 <ul>
     <?php foreach ($episodes as $episode): ?>
-        <li><a href="/podcast/<?= $episode->slug ?>"><?= $episode->title; ?></a></li>
+        <li><a href="/podcast/<?= $episode->url ?>"><?= $episode->title; ?></a></li>
     <?php endforeach; ?>
     <li><a href="/podcast">More ...</a></li>
 </ul>
