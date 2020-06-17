d4bb55f89ed744d65be96baf6ce5e5334e1f0d0d
DAB <dab@dab.la>
2020-06-17 09:51:32 -0400

    Added widescreen to podcast template default

--- Templates/episode.tpl.php
+++ Templates/episode.tpl.php
@@ -1,5 +1,5 @@
 <h1><?= $episode->title; ?></h1>
 <div class="responsive-embed widescreen">
-    <?= \lightningsdk\core\View\Video\HTML5::render('podcast'); ?>
+    <?= \lightningsdk\core\View\Video\HTML5::render('podcast', ['widescreen' => true]); ?>
 </div>
 <p><?= $episode->description; ?></p>
